e6cd6a2dce483588a984121d8681b3188c1fcd3a
Anghelo Alva <ance.anghelo@gmail.com>
2024-04-19 11:44:27 -0400

    Delete astrolicious i18n and use default Astro i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
-import i18n from '@astrolicious/i18n'
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    i18n({
-      defaultLocale: 'en',
-      locales: ['en', 'es'],
-    }),
-  ],
+  integrations: [tailwind()],
 })
