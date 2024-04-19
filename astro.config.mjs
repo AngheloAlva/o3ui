952bc450d1483642b0f3d9e15600239ca261fe75
Anghelo Alva <ance.anghelo@gmail.com>
2024-04-19 11:44:57 -0400

    Revert "Delete astrolicious i18n and use default Astro i18n"
    This reverts commit e6cd6a2dce483588a984121d8681b3188c1fcd3a.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
+import i18n from '@astrolicious/i18n'
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    i18n({
+      defaultLocale: 'en',
+      locales: ['en', 'es'],
+    }),
+  ],
 })
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,8 +13,8 @@ export default {
       black: {
         'base-100': '#1d232a',
         'base-200': '#191e24',
-        'primary': '#ff52d9',
         'secondary': '#7480ff',
+        'primary': '#ff52d9',
         'accent': '#00cdb7',
         'neutral': '#2a323c',
         'success': '#00a96e',
