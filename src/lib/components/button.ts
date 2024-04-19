56d3fffe7173312183e431da4d78a4e000ba9481
Anghelo Alva <ance.anghelo@gmail.com>
2024-04-19 19:38:34 -0400

    Add logic to read component data from ts archive + Install highlight.js to show code with color

---
+++ src/lib/components/button.ts
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: 'Theme Buttons',
+    code: [
+			`<button class="px-5 py-2.5 rounded-xl bg-primary duration-200 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-105 active:scale-y-90 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50">Primary</button>`,
+			`<button class="px-5 py-2.5 rounded-xl bg-secondary duration-200 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-105 active:scale-y-90 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-opacity-50">Secondary</button>`,
+			`<button class="px-5 py-2.5 rounded-xl bg-accent duration-200 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-105 active:scale-y-90 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-accent focus:ring-opacity-50">Accent</button>`,
+			`<button class="px-5 py-2.5 rounded-xl bg-neutral text-text duration-200 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-105 active:scale-y-90 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-neutral focus:ring-opacity-50">Neutral</button>`,
+			`<button class="px-5 py-2.5 rounded-xl bg-base-200 text-text duration-200 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-105 active:scale-y-90 hover:brightness-90 focus:outline-none focus:ring-2 focus:ring-base-200 focus:ring-opacity-50">Base 200</button>`,
+			`<button class="px-5 py-2.5 rounded-xl bg-transparent text-text duration-200 [transition-timing-function:cubic-bezier(0.175,0.885,0.32,1.275)] active:translate-y-1 active:scale-x-105 active:scale-y-90 hover:bg-base-200 hover:text-text focus:outline-none focus:ring-2 focus:ring-base-200 focus:ring-opacity-50">Ghost</button>`,
+			`<button class="px-5 py-2.5 rounded-xl bg-transparent text-primary duration-200 active:scale-x-90 active:scale-y-90 underline focus:outline-none focus:ring-2 focus:ring-primary focus:ring-opacity-50">Link</button>`,
+    ],
+  },
+]
---
+++ src/lib/interfaces/Component.ts
@@ -0,0 +1,4 @@
+export interface Component {
+	title: string
+	code: string
+}
